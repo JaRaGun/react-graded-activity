21d9b0c4d0bba120d87c66a7099bd09039955694
Jan Rafael Guno <guno.148983@pasay-edsa.sti.edu.ph>
2022-08-06 12:59:58 +0800

    change width

--- src/components/Animals.jsx
+++ src/components/Animals.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Animals({name,emoji}) {
   return (
-    <div className='p-4 border-amber-400 border-solid outline-2 border-2 w-28'>
+    <div className='p-4 border-amber-400 border-solid outline-2 border-2 w-32'>
         <h3>{name}</h3>
     </div>
   )
